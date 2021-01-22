1f3dfde9703d0184a71e6588fe24768fc25dcdfe
wildandhya <wildandhya15@gmail.com>
2021-01-22 13:40:56 +0700

    add new db

--- database/Jeager_db.sql
+++ database/Jeager_db.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 18, 2021 at 05:27 PM
--- Server version: 10.4.14-MariaDB
--- PHP Version: 7.2.33
+-- Generation Time: Jan 22, 2021 at 07:39 AM
+-- Server version: 10.4.13-MariaDB
+-- PHP Version: 7.4.7
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,8 +18,39 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `jeager`
+-- Database: `jeager_db`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `clients`
+--
+
+CREATE TABLE `clients` (
+  `clientId` varchar(255) NOT NULL,
+  `clientSecret` varchar(255) NOT NULL,
+  `redirectUri` varchar(255) NOT NULL,
+  `grants` set('','','','','') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `clients`
+--
+
+INSERT INTO `clients` (`clientId`, `clientSecret`, `redirectUri`, `grants`) VALUES
+('will.com', 'will.com Secret', 'http://localhost:8000/auth/callback', ''),
+('will.com', 'will.com Secret', 'http://localhost:8000/auth/callback', ''),
+('will.comm', 'will.comm Secret', 'http://localhost:8000/auth/callback', ''),
+('will.comm', 'will.comm Secret', 'http://localhost:8000/auth/callback', ''),
+('will.comm', 'will.comm Secret', 'http://localhost:8000/auth/callback', ''),
+('will.comm', 'will.comm Secret', 'http://localhost:8000/auth/callback', ''),
+('will.comm', 'will.comm Secret', 'http://localhost:8000/auth/callback', ''),
+('will.comm', 'will.comm Secret', 'http://localhost:8000/auth/callback', ''),
+('will.comm', 'will.comm Secret', 'http://localhost:8000/auth/callback', ''),
+('will.comm', 'will.comm Secret', 'http://localhost:8000/auth/callback', ''),
+('will.comm', 'will.comm Secret', 'http://localhost:8000/auth/callback', ''),
+('wildan', '123456', 'http://localhost:8000/auth/callback', '');
 
 -- --------------------------------------------------------
 
@@ -29,7 +60,7 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `roles` (
   `id` int(11) NOT NULL,
-  `name` varchar(266) NOT NULL
+  `name` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -37,11 +68,26 @@ CREATE TABLE `roles` (
 --
 
 INSERT INTO `roles` (`id`, `name`) VALUES
-(1, 'Super Admin'),
+(1, 'Super admin'),
 (2, 'Admin'),
 (3, 'Director'),
 (4, 'Head of engineering'),
 (5, 'Operator');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tokens`
+--
+
+CREATE TABLE `tokens` (
+  `accessToken` varchar(255) NOT NULL,
+  `accessTokenExpiresAt` date NOT NULL,
+  `refreshToken` varchar(255) NOT NULL,
+  `refreshTokenExpiresAt` date NOT NULL,
+  `client_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -51,9 +97,9 @@ INSERT INTO `roles` (`id`, `name`) VALUES
 
 CREATE TABLE `user` (
   `id` int(11) NOT NULL,
-  `name` varchar(266) NOT NULL,
-  `email` varchar(266) NOT NULL,
-  `password` varchar(266) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
   `user_role` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -71,8 +117,7 @@ ALTER TABLE `roles`
 -- Indexes for table `user`
 --
 ALTER TABLE `user`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `user_role` (`user_role`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -83,22 +128,6 @@ ALTER TABLE `user`
 --
 ALTER TABLE `roles`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
-
---
--- AUTO_INCREMENT for table `user`
---
-ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `user`
---
-ALTER TABLE `user`
-  ADD CONSTRAINT `user_ibfk_1` FOREIGN KEY (`user_role`) REFERENCES `roles` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
